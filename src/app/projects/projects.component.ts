11dfa73f32d89343db8dbf1ede2b23fc0c04a3cc
unknown <mateusz.figon97@gmail.com>
2020-09-18 19:49:10 +0200

    borders, project card multiple projects card and selected project card added, font weights changed

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as projects from '../projects.json';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
-
+  projectsArray = [];
+  currentProject = null;
   ngOnInit(): void {
+    this.projectsArray= (projects as any).default;
+    console.log(this.projectsArray);
+  }
+  showProject(project){
+    this.currentProject=project;
   }
 
 }
